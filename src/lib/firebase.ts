f9515040e232dec5d020a4c666a19de9457dfce9
Mangesh Ahirrao <mahirrao925@gmail.com>
2023-12-10 18:30:01 -0600

    initial commit

---
+++ src/lib/firebase.ts
@@ -0,0 +1,35 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "taskmate-ai-63ae2.firebaseapp.com",
+  projectId: "taskmate-ai-63ae2",
+  storageBucket: "taskmate-ai-63ae2.appspot.com",
+  messagingSenderId: "802936167278",
+  appId: "1:802936167278:web:dde0f5aea2e75bc9a37f7f",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
+
+export async function uploadFileToFirebase(image_url: string, name: string) {
+  try {
+    const response = await fetch(image_url);
+    const buffer = await response.arrayBuffer();
+    const file_name = name.replace(" ", "") + Date.now + ".jpeg";
+    const storageRef = ref(storage, file_name);
+    await uploadBytes(storageRef, buffer, {
+      contentType: "image/jpeg",
+    });
+    const firebase_url = await getDownloadURL(storageRef);
+    return firebase_url;
+  } catch (error) {
+    console.error(error);
+  }
+}
